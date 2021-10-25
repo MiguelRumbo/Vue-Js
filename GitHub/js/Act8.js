06fb878bb2690e63d126aefb625da1e56825b427
Miguel Rumbo <86128109+MiguelRumbo@users.noreply.github.com>
2021-10-24 23:11:58 -0500

    Index

---
+++ GitHub/js/Act8.js
@@ -0,0 +1,52 @@
+new Vue({
+    el: '#article',
+    data: {
+        articles: [
+            { cant: 5, price_unit: 869, price_items: 0 },
+            { cant: 2, price_unit: 349, price_items: 0 }
+        ],
+        subtotal: 0,
+        iva: 0,
+        iva_porcentaje: .15,
+        total: 0,
+    },
+    methods: {
+        price_calc(item) {
+            item.price_items = item.cant * item.price_unit;
+            return item.price_items;
+        },
+        add_Art() {
+            this.articles.push({
+                cant: '',
+                price_unit: '',
+                price_items: ''
+            })
+        }
+    },
+    computed: {
+        Subtotal() {
+            this.total = 0;
+            for (obj of this.articles) {
+                this.total += obj.price_items;
+            }
+            this.iva = this.total * this.iva_porcentaje;
+            this.subtotal = this.total - this.iva;
+            return this.subtotal
+        },
+        Iva() {
+            this.total = 0;
+            for (obj of this.articles) {
+                this.total += obj.price_items;
+            }
+            this.iva = this.total * this.iva_porcentaje;
+            return this.iva.toFixed(2)
+        },
+        Totalprice() {
+            this.total = 0;
+            for (obj of this.articles) {
+                this.total += obj.price_items;
+            }
+            return this.total
+        }
+    }
+});
